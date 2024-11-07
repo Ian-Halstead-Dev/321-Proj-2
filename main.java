16ecec3ea30069b35771b2e3aa78f020672dd80f
Ian Halstead <ihalstea@iastate.edu>
2024-11-06 22:21:52 -0600

    All finished for real

--- main.java
+++ main.java
@@ -37,37 +37,17 @@ class Main {
 
     instructionMap.put(0b10001011000, "ADD");
     instructionMap.put(0b1001000100, "ADDI");
-    instructionMap.put(0b1011000100, "ADDIS:");
-    instructionMap.put(0b10101011000, "ADDS");
     instructionMap.put(0b10001010000, "AND");
     instructionMap.put(0b1001001000, "ANDI");
-    instructionMap.put(0b1111001000, "ANDIS:");
-    instructionMap.put(0b1110101000, "ANDS");
     instructionMap.put(0b000101, "B");
     instructionMap.put(0b100101, "BL");
     instructionMap.put(0b11010110000, "BR");
     instructionMap.put(0b10110101, "CBNZ");
     instructionMap.put(0b10110100, "CBZ");
-    instructionMap.put(0b11111111110, "DUMP");
     instructionMap.put(0b11001010000, "EOR");
     instructionMap.put(0b1101001000, "EORI");
-    instructionMap.put(0b00011110011, "FADDD");
-    instructionMap.put(0b00011110001, "FADDS");
-    instructionMap.put(0b00011110011, "FCMPD");
-    instructionMap.put(0b00011110001, "FCMPS");
-    instructionMap.put(0b00011110011, "FDIVD");
-    instructionMap.put(0b00011110001, "FDIVS");
-    instructionMap.put(0b00011110011, "FMULD");
-    instructionMap.put(0b00011110001, "FMULS");
-    instructionMap.put(0b00011110011, "FSUBD");
-    instructionMap.put(0b00011110001, "FSUBS");
     instructionMap.put(0b11111111111, "HALT");
-    instructionMap.put(0b11111000010, "LDUR:");
-    instructionMap.put(0b00111000010, "LDURB:");
-    instructionMap.put(0b11111100010, "LDURD:");
-    instructionMap.put(0b01111000010, "LDURH:");
-    instructionMap.put(0b10111100010, "LDURS:");
-    instructionMap.put(0b10111000100, "LDURSW:");
+    instructionMap.put(0b11111000010, "LDUR");
     instructionMap.put(0b11010011011, "LSL");
     instructionMap.put(0b11010011010, "LSR");
     instructionMap.put(0b10011011000, "MUL");
@@ -75,20 +55,12 @@ class Main {
     instructionMap.put(0b1011001000, "ORRI");
     instructionMap.put(0b11111111100, "PRNL");
     instructionMap.put(0b11111111101, "PRNT");
-    instructionMap.put(0b10011010110, "SDIV");
-    instructionMap.put(0b10011011010, "SMULH");
-    instructionMap.put(0b11111000000, "STUR:");
-    instructionMap.put(0b00111000000, "STURB:");
-    instructionMap.put(0b11111100000, "STURD:");
-    instructionMap.put(0b01111000000, "STURH:");
-    instructionMap.put(0b10111100000, "STURS:");
-    instructionMap.put(0b10111000000, "STURSW:");
+    instructionMap.put(0b11111000000, "STUR");
     instructionMap.put(0b11001011000, "SUB");
     instructionMap.put(0b1101000100, "SUBI");
     instructionMap.put(0b1111000100, "SUBIS");
     instructionMap.put(0b11101011000, "SUBS");
-    instructionMap.put(0b10011010110, "UDIV");
-    instructionMap.put(0b10011011110, "UMULH");
+    instructionMap.put(0b11111111110, "DUMP");
     instructionMap.put(0b01010100, "B.");
     return instructionMap;
   }
@@ -151,28 +123,40 @@ class Main {
       else if (instructionMap.containsKey(instruction.op_10)) {
         String instructionString = instructionMap.get(instruction.op_10);
         //I Type
-        strToPrint.add(instructionString + " X" + instruction.rd + ", X" + instruction.rn + ", #" + convertTo2s(instruction.aluImmediate,12));
+        strToPrint.add(instructionString + " " + printRegister(instruction.rd) + ", " + printRegister(instruction.rn) + ", #" + convertTo2s(instruction.aluImmediate,12));
       }
       else if (instructionMap.containsKey(instruction.op_11)) {
         String instructionString = instructionMap.get(instruction.op_11);
-        if(instructionString.charAt(instructionString.length() - 1) == ':') {
+        if(instruction.op_11 == 0b11111000000 || instruction.op_11 == 0b11111000010) {
           // D type
           instructionString = instructionString.substring(0, instructionString.length() - 1);
-          strToPrint.add(instructionString + " X" + instruction.rd + ", [X" + instruction.rn + ", #" + instruction.DT_address + "]");
+          strToPrint.add(instructionString + " " + printRegister(instruction.rd) + ", [" + printRegister(instruction.rn) + ", #" + instruction.DT_address + "]");
         }
-        else {
-          if("LSL".equals(instructionString) || "LSR".equals(instructionString)) {
+          else if(instruction.op_11 == 0b11010011011 || instruction.op_11 == 0b11010011010) {
             // R type (Shift)
-            strToPrint.add(instructionString + " X" + instruction.rd + ", X" + instruction.rn + ", #" + instruction.shamt);
+            strToPrint.add(instructionString + " " + printRegister(instruction.rd) + ", " + printRegister(instruction.rn) + ", #" + instruction.shamt);
           }
-          else {
+            else if(instruction.op_11 == 0b11010110000) {
+              // BR
+              strToPrint.add(instructionString + " " + printRegister(instruction.rn));
+            }
+            else if(instruction.op_11 == 0b11111111100|| instruction.op_11 == 0b11111111110|| instruction.op_11 == 0b11111111111) {
+              strToPrint.add(instructionString);
+            }
+            else if(instruction.op_11 == 0b11111111101) {
+              strToPrint.add(instructionString + " " + printRegister(instruction.rd));
+            }
             // R type
-            strToPrint.add(instructionString + " X" + instruction.rd + ", X" + instruction.rn + ", X" + instruction.rm);
-          }
+            else {
+              strToPrint.add(instructionString + " " + printRegister(instruction.rd) + ", " + printRegister(instruction.rn) + ", " + printRegister(instruction.rm));
+            }
           
-        }
+          
+        
 
       }
+      else {System.err.println("Invalid instruction! Instruction " + i + " is invalid!");
+    return;}
     }
     
     List<Map.Entry<Integer, Integer>> entryList = new ArrayList<>(labelMap.entrySet());
@@ -203,11 +187,21 @@ class Main {
     }
 
     return maskedNum;
+  } 
+
+  static String printRegister(int register) {
+    if(register == 31) {
+      return "XZR";
+    }
+    if(register == 28) {
+      return "SP";
+    }
+    if (register == 29) {
+      return "FP";
+    }
+    if(register == 30) {
+      return "LR";
+    }
+    return "X" + register;
+  }
 }
-
-}
-
-
-
-// 1001 0001 0010 0000 0010 0011 1110 0000
-// 00010111111111111111111111111111
